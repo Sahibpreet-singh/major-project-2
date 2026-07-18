9327f174a5f06da1dba89701283834bbab7e67a8
Sahibpreet-singh <165685049+Sahibpreet-singh@users.noreply.github.com>
2026-07-18 17:42:30 +0530

    final__

--- main.py
+++ main.py
@@ -31,9 +31,17 @@ from backend.ai.schemas import ChatRequest
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://major-project-2-689dnt6ok-sahibpreet-singhs-projects.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https://.*\.vercel\.app",
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
